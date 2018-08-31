deaf721904739cfce03ac1ee30a0cd2c75bdaf11
Your Name <18817957631@163.com>
2018-08-31 09:01:21 +0800

    reset

--- test/pratest.cpp
+++ test/pratest.cpp
@@ -6,48 +6,30 @@
 #include <string.h>
 #include <pthread.h>
 #include <fstream>
-#include "test.pb.h"
+#include <sys/time.h>
+#include "../src/test.pb.h"
 using namespace std;
 #define BUFFSIZE 1024
 int main()
 {
-  GOOGLE_PROTOBUF_VERIFY_VERSION;
-  //写文件
-  lm::helloworld msg1;
-  msg1.set_id(1);
-  msg1.set_str("test1");
-  fstream output("./log", ios::out | ios::trunc | ios::binary);
-  if (!msg1.SerializeToOstream(&output)) {
-      cerr << "Failed to write msg." << endl;
-      return -1;
-  }
-  output.close();
-  //读文件
-  lm::helloworld msg2;
-  fstream input("./log", ios::in | ios::binary);
-  if (!input)
-  {
-     cerr << "open file failed!\n";
-     return -1;
-  }
-  if (!msg2.ParseFromIstream(&input)) {
-      cerr << "Parse file failed!" << endl;
-      return -1;
-  }
-  input.close();
-  for(int i=0;i<100000;i++)
+  clock_t start,end;
+  start = clock();
+  for(int i=0;i<1000000;i++)
   {
    // 写buf, protobuf序列化
-   lm::helloworld msg3;
-   msg3.set_id(2);
-   msg3.set_str("test2");
+   lm::helloworld msg1;
+   msg1.set_id(2);
+   msg1.set_str("test2");
    char buf[BUFFSIZE];
    memset(buf,0,BUFFSIZE);
-   msg3.SerializeToArray(buf,BUFFSIZE);
+   msg1.SerializeToArray(buf,BUFFSIZE);
    //读buf,protobuf反序列化
-   lm::helloworld msg4;
-   msg4.ParseFromArray(buf, BUFFSIZE);
+   lm::helloworld msg2;
+   msg2.ParseFromArray(buf, BUFFSIZE);
    google::protobuf::ShutdownProtobufLibrary();
   }
+  end = clock();
+  double seconds = (double)(end - start)/CLOCKS_PER_SEC;
+  fprintf(stderr, "Use time is: %.8f\n", seconds);
   return 0;
 }
